7409e2dcdcefaa19d8582d4404b189430614fb9a
Tu Tv <tutv95@gmail.com>
2016-06-17 23:33:43 +0700

    Add static files

---
+++ public/assets/js/main.js
@@ -0,0 +1,42 @@
+jQuery(document).ready(function ($) {
+    $('body').on('valid.bs.validator', function (event) {
+        var x = event.relatedTarget.outerHTML;
+        if (x.indexOf('id="msv"') >= 0) {
+            loadTimetable();
+        }
+    });
+
+    var msv = $('#msv').val();
+    if (msv != '' && msv.length == 8) {
+        loadTimetable();
+    }
+
+    function loadTimetable() {
+        var msv = $('#msv').val();
+        var url = url_ajax + '?msv=' + msv;
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: function (data) {
+                var time = data.timetable;
+                var nameSV = data.name;
+                var htmlTable = '';
+                for (var i = 0; i < time.length; i++) {
+                    var code = time[i].code;
+                    var name = time[i].name;
+                    var stt = i + 1;
+
+                    htmlTable += '<tr>' +
+                        '<td>' + stt + '</td>'
+                        + '<td>' + code + '</td>'
+                        + '<td>' + name + '</td>'
+                        + '</tr>';
+                }
+
+                $('#name').val(nameSV);
+                $('#timetable tbody').html(htmlTable);
+            }
+        });
+    }
+});
