d916de62333da4febcd9b1b00f51de334855e198
Tu Tv <tutv95@gmail.com>
2016-06-19 00:34:22 +0700

    Updated bot

--- connect-bot.js
+++ connect-bot.js
@@ -3,31 +3,24 @@
 module.exports = class BotK {
 
     constructor() {
-        this.data = new ResponseK();
         this.host = 'http://128.199.223.91:8080/uetbot/api?apiKey=roseisred_violetisblue_uet14020577';
         this.request = require('unirest');
     }
 
     newUser(msv, email) {
-        var data = this.data.newUser(msv, email);
+        var data = DataK.newUser(msv, email);
 
         return this.createRequest().send(data);
     }
 
-    hasScore(name, code, link, members) {
-        var data = this.data.hasScore(name, code, link, members);
-
-        return this.createRequest(data)
-    }
-
-    createRequest(data) {
+    private createRequest(data) {
         return this.request.post(this.host)
             .type('json').send(data);
     }
 };
 
-class ResponseK {
-    newUser(msv, email) {
+class DataK {
+    static newUser(msv, email) {
         var md5 = require('md5');
 
         return {
@@ -35,15 +28,5 @@ class ResponseK {
             mssv: msv,
             token: md5(email)
         };
-    }
-
-    hasScore(name, code, link, members) {
-        return {
-            type: 'newgrade',
-            course_name: name,
-            course_code: code,
-            grade_link: link,
-            members: members
-        }
     }
 }
